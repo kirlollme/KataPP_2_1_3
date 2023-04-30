5897cd660349343636f8e1a2819fcd6f372b865b
Kirill <kir.adam2000@gmail.com>
2023-04-30 18:25:59 +0300

    Решенная задача

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Результат сравнения Cat бинов");
+        System.out.println( cat2.equals(cat1));
+        System.out.println("Результат сравнения HelloWorld бинов");
+        System.out.println( bean.equals(bean1));
     }
 }
